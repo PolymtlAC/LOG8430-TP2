a4ba5d0d5f789108868fb827687e53c89be76808
alePereira <alexandre.pereira@polymtl.ca>
2016-03-11 14:59:20 -0500

    ajout du scrollPane pour le container des commandes avec mise a jour de la taille en fonction des ajouts de commandes, ajout du listener pour Clear et de la fonction de detection d'etat pour autorun

--- tp2/src/com/log8430/group9/views/CommandsPart.java
+++ tp2/src/com/log8430/group9/views/CommandsPart.java
@@ -3,26 +3,36 @@ package com.log8430.group9.views;
 import javax.annotation.PostConstruct;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 
 public class CommandsPart {
 	protected Composite commandsContainer;
 	protected Button clear;
 	protected Button autoRun;
-	
-	 @PostConstruct
+
+	@PostConstruct
 	public void createComposite(Composite parent) {
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.numColumns = 2;
-		gridLayout.makeColumnsEqualWidth = true;
-		parent.setLayout(gridLayout);
-		
-		commandsContainer = new Composite(parent, SWT.NONE);
-		GridData gridDataCommandsContainer = new GridData(GridData.FILL,GridData.FILL,true,true,2,4);
-		commandsContainer.setLayoutData(gridDataCommandsContainer);
+		parent.setLayout(gridLayout);	
+
+
+		final ScrolledComposite scrollPane = new ScrolledComposite(parent, SWT.V_SCROLL | SWT.BORDER);
+		GridData gridDataScrollPane = new GridData(GridData.FILL,GridData.FILL,true,true,3,4);
+		scrollPane.setLayoutData(gridDataScrollPane);
+
+		commandsContainer = new Composite(scrollPane, SWT.NONE);
+		scrollPane.setContent(commandsContainer);
+		GridLayout gridLayoutCC = new GridLayout();
+		gridLayoutCC.numColumns = 1;
+		commandsContainer.setLayout(gridLayoutCC);
+		commandsContainer.setSize(commandsContainer.computeSize(SWT.BORDER, SWT.DEFAULT));
 		
 		clear = new Button(parent, SWT.PUSH);
 		clear.setText("Clear");
@@ -30,8 +40,28 @@ public class CommandsPart {
 		clear.setLayoutData(gridDataClear);
 		autoRun = new Button(parent, SWT.CHECK);
 		autoRun.setText("autoRun");
-		GridData gridDataAutoRun = new GridData(GridData.CENTER,GridData.FILL,false,false,1,1);
+		GridData gridDataAutoRun = new GridData(GridData.FILL,GridData.FILL,false,false,1,1);
 		autoRun.setLayoutData(gridDataAutoRun);
 		
+		
+		//TODO cette action est pour tester la mise a jour des dimmension du scroll a l'ajout d'element
+		//il faudra utiliser son contenu lors du chargement des commandes
+		clear.addListener(SWT.Selection, new Listener() {
+				@Override
+				public void handleEvent(Event e) {	              
+	              Button button = new Button(commandsContainer, SWT.PUSH);
+	              button.setText("button ");
+	              // reset the minimum width and height so children can be seen - method 2
+	              commandsContainer.setSize(commandsContainer.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+	              commandsContainer.layout();
+	          }
+	      });
+		
+		
+		//permet de connaitre l'état dans lequel se trouve le bouton
+		//autoRun.getSelection();
+		
+		
+
 	}
 }
